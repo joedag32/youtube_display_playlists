a9f8d5c78f1330dafed3cf894a3e741b8edb37ec
Joseph D'Agostino <Joseph.D'Agostino@stockton.edu>
2015-10-02 14:08:39 -0400

    playlist script

---
+++ youtube_display_playlist.js
@@ -0,0 +1,22 @@
+// set api key, playlistid and max_results variables
+var youtube_key = 'AIzaSyBrm7aHBhMvjDhJFaj0s9bXXEd45w7YfrE';
+var youtube_playlist_id = 'PLq3J_zhDNwnkHgSEXbX_hvrvv6v8jsdMv'; 
+var youtube_max_results = '50';
+
+// create json rest feed
+var youtube_rest_feed = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=' + youtube_max_results + '&playlistId=' + youtube_playlist_id + '&key=' + youtube_key;
+
+// load and parse the feed
+$.getJSON(youtube_rest_feed, function(playlistData) {
+  var output="<ul>";
+  for (var i in playlistData.items) {
+    var videoTitle = "<h3><a href='https://www.youtube.com/watch?v=" + playlistData.items[i].snippet.resourceId.videoId + "'>" + playlistData.items[i].snippet.title + "</a></h3>";
+    var videoThumbnail = "<a href='https://www.youtube.com/watch?v=" + playlistData.items[i].snippet.resourceId.videoId + "'><img src='" + playlistData.items[i].snippet.thumbnails.default.url + "' alt='" + playlistData.items[i].snippet.title + "' width='120' height='90' /></a>";
+    var videoDescription = "<p>" + playlistData.items[i].snippet.description + "</p>"
+    var publishedDate = new Date(Date.parse(playlistData.items[i].snippet.publishedAt));
+    var publishedDateFormatted = "<p>" + publishedDate.toString() + "</p>";
+    output += "<li>" + videoThumbnail + videoTitle + videoDescription + "</li>";
+  }
+  output+="</ul>";
+  $("#video-container").append(output);
+});
